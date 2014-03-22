c563f09a270183d6278956f3bc71d87b5b8b208a
Nicola Nauywnck <nicolanauwynck@hotmail.com>
2014-03-22 18:21:12 +0100

    update

--- scripts/database_connection.js
+++ scripts/database_connection.js
@@ -1,2 +1,17 @@
 ﻿//var url = "http://localhost:57320/";
-var url = "http://forgather.azurewebsites.net/";
+//var url = "http://forgather.azurewebsites.net/";
+
+var url;
+
+$.ajax({
+    datatype: 'json',
+    url: "http://localhost:57320/api/friend/getfriends?userID=5",
+    data: null,
+    async: false,
+    success: function (data) {
+        url = "http://localhost:57320/";
+    },
+    error: function (e) {
+        url = "http://forgather.azurewebsites.net/";
+    }
+});
